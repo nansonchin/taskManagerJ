729daaeff0eb7435d7f13ea862d15b0a4f50f2a4
JG Chin <guoc71@gmail.com>
2026-09-15 15:01:35 +0800

    test: useEffect

--- src/pages/IssuesPage.tsx
+++ src/pages/IssuesPage.tsx
@@ -1,29 +1,57 @@
-import { useEffect, useState } from "react"
-import fetchIssues from "../api/fetchIssues"
+import { useEffect, useState } from "react";
+import fetchIssues from "../api/fetchIssues";
 
-export default function IssuesPage(){
-    const [isLoading,setIsLoading] = useState(false)
-    const [issues, setIssues] = useState([])
-    const [error,setIsError] = useState(false)
-    const [errorMessage,setErrorMessage] = useState("")
+export default function IssuesPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [issues, setIssues] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(()=>{
-        setIsLoading(true)
-        setIsError(false)
-        const loadIssues =async () =>{
-            try{
-                const result = await fetchIssues()
-                if(result){
-                    setIssues(result)
-                    setIsLoaading(false)
-                }
-            }catch(error:any){
-                setIsError(true)
-                setIsLoaading(false)
-                setErrorMessage(error.message)
-            }
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+    const loadIssues = async () => {
+      try {
+        const result = await fetchIssues();
+        setIssues(result);
+        setIsLoading(false);
+      } catch (error: any) {
+        setIsError(true);
+        setIsLoading(false);
+        setErrorMessage(error.message);
+      }
+    };
+
+    loadIssues();
+  }, []);
+
+  if(isLoading){
+    return(
+        <div>
+            Loading ...
+        </div>
+    )
+  }
+
+  if(isError){
+    return(
+        <div>
+            Something went wrong
+        </div>
+    )
+  }
+
+  return(
+    <div>
+        {
+            issues.forEach((issue)=>{
+                return (
+                    <div>
+                        {issue}
+                        </div>
+                )
+            })
         }
-
-        loadIssues()
-    },[])
+    </div>
+  )
 }
