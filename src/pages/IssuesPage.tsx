6b9fd0de584ba53492c221792967bce9f7acc1f4
JG Chin <guoc71@gmail.com>
2026-09-15 21:37:21 +0800

    test: IssuesPage

--- src/pages/IssuesPage.tsx
+++ src/pages/IssuesPage.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import fetchIssues from "../api/fetchIssues";
+import getNextStatus from "../utils/getNextStatus";
+
 
 export default function IssuesPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [issues, setIssues] = useState([]);
   const [isError, setIsError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
 
   useEffect(() => {
     setIsLoading(true);
@@ -25,6 +28,23 @@ export default function IssuesPage() {
 
     loadIssues();
   }, []);
+
+  
+  const handleStatusChange = (issueId) =>{
+    const issue = issues.find((data)=>data.id === issueId)
+    const nextStatus = getNextStatus(issue.status)
+
+    setIssues(
+      issues.map((data)=>{
+        if(data.id === issueId){
+          return{
+            ...data,
+            status:nextStatus
+          }
+        }
+      })
+    )
+  }
 
   if (isLoading) {
     return <div>Loading ...</div>;
